c9747cff057664352b12f2ee26a2ba1c7a7e8b0b
mijip0320 <39548731+mijip0320@users.noreply.github.com>
2025-02-07 07:50:35 +0900

    feat : nodeExpress 생성

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+const friends = [
+    {
+        id: 0,
+        name : 'Albert Einstien'
+    },
+    {
+        id: 1,
+        name : 'Sir Isaac Newto0n'
+    }
+];
+
+app.get('/friends', (req,res) => {
+    res.json(friends);
+})
+
+app.get('/friends/:friendId', (req, res) => {
+    const friendId = Number(req.params.friendId);
+    const friend = friends[friendId];
+    if (friend) {
+        res.status(202).json(friend);
+    } else {
+        res.status(404).json({
+            error : "Friend does not exist"
+        });
+    }
+})
+
+app.get('/messages', (req,res) => {
+    res.send('<ul><li>HELLO MIJI PARK</li></ul>');
+})
+
+app.post('/messages', (req,res) => {
+   console.log('UPDATING MESSAGES')
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Listeneing on ${PORT}...`);
+})
